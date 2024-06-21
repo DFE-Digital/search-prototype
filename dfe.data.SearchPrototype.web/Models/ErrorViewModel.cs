b22662a79f2ceb83cfa23b0a0a9addbdba701494
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-06-21 20:31:39 +0100

    initial commit

---
+++ dfe.data.SearchPrototype.web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace data.search_prototype.web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
