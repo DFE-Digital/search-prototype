c2a6c05adeef4d289c2a35fbbd40aed869bfa086
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-01 10:49:27 +0100

    Removed legacy project structure

--- dfe.data.SearchPrototype.web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Dfe.Data.SearchPrototype.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
