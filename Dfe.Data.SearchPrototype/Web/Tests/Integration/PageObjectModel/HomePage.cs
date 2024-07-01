73503896ecace38f78a8a85a9848976b5e2cb860
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-01 13:19:14 +0100

    Initial commit with revised page model

---
+++ Dfe.Data.SearchPrototype/Web/Tests/Integration/PageObjectModel/HomePage.cs
@@ -0,0 +1,30 @@
+﻿using AngleSharp.Html.Dom;
+using DfE.Data.SearchPrototype.Web.Tests.Integration.PageObjectModel.PageComponents;
+using DfE.Data.SearchPrototype.Web.Tests.Integration.PageObjectModel.Setup;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DfE.Data.SearchPrototype.Web.Tests.Integration.PageObjectModel
+{
+    public sealed class HomePage : PageObjectModelExtractor
+    {
+        private readonly PageHeader _pageHeader;
+
+        public HomePage(WebApplicationFactory<Program> webApplicationFactory) :
+            base(webApplicationFactory)
+        {
+            _pageHeader =
+                PageHeader.Create(webApplicationFactory, PageName);
+        }
+
+        public string GetHomePageHeading() =>
+            _pageHeader.GetMainHeading("govuk-header__link govuk-header__service-name");
+
+        public IHtmlAnchorElement GetHomePageHeaderLink() =>
+            _pageHeader.GetHeaderLink(linkName: "Privacy");
+
+        private const string PageName = "Home";
+
+        public static HomePage Create(
+            WebApplicationFactory<Program> webApplicationFactory) => new(webApplicationFactory);
+    }
+}
