7d06c2d59a6f573ece525d3eb9f0c3d99fd02d88
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-01 10:41:37 +0100

    Removed page object model structures - clean merge from main

---
+++ Dfe.Data.SearchPrototype/Web/Tests/Integration/Shared/PageTestHelper.cs
@@ -0,0 +1,27 @@
+﻿using AngleSharp;
+using AngleSharp.Dom;
+using Microsoft.AspNetCore.Mvc.Testing;
+
+namespace DfE.Data.SearchPrototype.Test.Shared;
+
+public class PageTestHelper : IntegrationTestingWebApplicationFactory
+{
+    private IBrowsingContext _context;
+
+    public PageTestHelper(WebApplicationFactory<Program> factory) : base(factory)
+    {
+        // anglesharp
+        AngleSharp.IConfiguration angleSharpConfig = Configuration.Default;
+        _context = BrowsingContext.New(angleSharpConfig);
+        //--------------------------------------------------------------------
+    }
+
+    protected async Task<IDocument> NavigateToPage(string webPage)
+    {
+        HttpResponseMessage response = await _httpClient.GetAsync(webPage);
+
+        var DOM = await response.Content.ReadAsStringAsync();
+
+        return await _context.OpenAsync(req => req.Content(DOM));
+    }
+}
---
+++ Dfe.Data.SearchPrototype/Web/Tests/Integration/readme.md
@@ -0,0 +1,4 @@
+﻿# Project description 
+
+These integration tests run the web app and use Anglesharp to query the DOM of the returned page for expected elements.
+
