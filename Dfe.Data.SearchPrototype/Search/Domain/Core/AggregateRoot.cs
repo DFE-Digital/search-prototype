f0ed39319e5c33a406eab1c253f544041d7adb3d
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-03 15:32:33 +0100

    Added scaffolding for core domain constructs

---
+++ Dfe.Data.SearchPrototype/Search/Domain/Core/AggregateRoot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dfe.Data.SearchPrototype.Search.Domain.Core
+{
+    internal class AggregateRoot
+    {
+    }
+}
