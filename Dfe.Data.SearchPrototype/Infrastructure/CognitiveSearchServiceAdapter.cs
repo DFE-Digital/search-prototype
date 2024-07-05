c583c91de078a17b343c4df4110ab0a7f3555257
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-05 08:51:28 +0100

    Added comments to the CognitiveSearchServiceAdapter.

--- Dfe.Data.SearchPrototype/Infrastructure/CognitiveSearchServiceAdapter.cs
+++ Dfe.Data.SearchPrototype/Infrastructure/CognitiveSearchServiceAdapter.cs
@@ -10,7 +10,8 @@ using DfE.Data.ComponentLibrary.Infrastructure.CognitiveSearch.Search;
 namespace Dfe.Data.SearchPrototype.Infrastructure
 {
     /// <summary>
-    /// 
+    /// Provides an adaption of the core Azure cognitive search services to allow
+    /// compatability with the Dfe.Data.SearchPrototype application service defintion.
     /// </summary>
     public sealed class CognitiveSearchServiceAdapter : ISearchServiceAdapter
     {
@@ -19,11 +20,12 @@ namespace Dfe.Data.SearchPrototype.Infrastructure
         private readonly IMapper<Response<SearchResults<object>>, Establishments> _searchResponseMapper;
 
         /// <summary>
-        /// 
+        /// The following dependencies include the core cognitive search service definition,
+        /// the complete implementation of which is defined in the IOC container.
         /// </summary>
-        /// <param name="cognitiveSearchService"></param>
-        /// <param name="searchOptionsFactory"></param>
-        /// <param name="searchResponseMapper"></param>
+        /// <param name="cognitiveSearchService">Cognitive search service defintion injected via IOC container.</param>
+        /// <param name="searchOptionsFactory">Factory class definition for prescribing the requested search options (by collection context).</param>
+        /// <param name="searchResponseMapper">Maps the raw azure search response to the required "T:Dfe.Data.SearchPrototype.Search.Domain.AgregateRoot.Establishments"</param>
         public CognitiveSearchServiceAdapter(
             ISearchService cognitiveSearchService,
             ISearchOptionsFactory searchOptionsFactory,
@@ -35,11 +37,15 @@ namespace Dfe.Data.SearchPrototype.Infrastructure
         }
 
         /// <summary>
-        /// 
+        /// Makes call to underlying azure cognitive search service and uses the prescribed mapper
+        /// to adapt the raw asure search results to the "T:Dfe.Data.SearchPrototype.Search.Domain.AgregateRoot.Establishments" type.
         /// </summary>
-        /// <param name="searchContext"></param>
-        /// <returns></returns>
-        /// <exception cref="ApplicationException"></exception>
+        /// <param name="searchContext">Prescribes the context of the serach including the keywork and collection target.</param>
+        /// <returns>
+        /// A configured "T:Dfe.Data.SearchPrototype.Search.Domain.AgregateRoot.Establishments"
+        /// object hydrated from the results of the azure search.
+        /// </returns>
+        /// <exception cref="ApplicationException">The exception thrown</exception>
         public async Task<Establishments> Search(SearchContext searchContext)
         {
             SearchOptions searchOptions =
