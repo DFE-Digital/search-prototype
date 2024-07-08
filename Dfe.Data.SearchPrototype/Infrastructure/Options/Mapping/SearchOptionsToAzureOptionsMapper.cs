fb79522edefdb502242dde9ee357ec2b584dfa7d
Spencer O'HEGARTY <Spencer.OHEGARTY@EDUCATION.GOV.UK>
2024-07-08 14:02:19 +0100

    Added comments to options constructs.

--- Dfe.Data.SearchPrototype/Infrastructure/Options/Mapping/SearchOptionsToAzureOptionsMapper.cs
+++ Dfe.Data.SearchPrototype/Infrastructure/Options/Mapping/SearchOptionsToAzureOptionsMapper.cs
@@ -4,15 +4,25 @@ using DfE.Data.ComponentLibrary.CrossCuttingConcerns.Mapping;
 namespace Dfe.Data.SearchPrototype.Infrastructure.Options.Mapping;
 
 /// <summary>
-/// 
+/// Concrete mapper implementaiton which translates the results returned from the internal
+/// "T:Dfe.Data.SearchPrototype.Infrastructure.Options.SearchSettingsOptions",
+/// and maps them to the "T:Azure.Search.Documents.SearchOptions"
+/// type required to configure and invoke the Azure search.
 /// </summary>
 public sealed class SearchOptionsToAzureOptionsMapper : IMapper<SearchSettingsOptions, SearchOptions>
 {
     /// <summary>
-    /// 
+    /// Entry point for mapping from the recieved
+    /// "T:Dfe.Data.SearchPrototype.Infrastructure.Options.SearchSettingsOptions"
+    /// to the returned "T:Azure.Search.Documents.SearchOptions" instance.
     /// </summary>
-    /// <param name="input"></param>
-    /// <returns></returns>
+    /// <param name="input">
+    /// The internally configured search settings which
+    /// seed the "T:Azure.Search.Documents.SearchOptions" mapping.
+    /// </param>
+    /// <returns>
+    /// A fully configured "T:Azure.Search.Documents.SearchOptions" instance.
+    /// </returns>
     public SearchOptions MapFrom(SearchSettingsOptions input)
     {
         var searchOptions = new SearchOptions()
--- Dfe.Data.SearchPrototype/Infrastructure/Options/SearchOptionsFactory.cs
+++ Dfe.Data.SearchPrototype/Infrastructure/Options/SearchOptionsFactory.cs
@@ -5,7 +5,8 @@ using Microsoft.Extensions.Options;
 namespace Dfe.Data.SearchPrototype.Infrastructure.Options;
 
 /// <summary>
-/// 
+/// Concrete implementation for deriving "T:Dfe.Data.SearchPrototype.Infrastructure.Options.SearchSettingsOptions"
+/// configuration snapshots and mapping them to the expected "T:Azure.Search.Documents.SearchOptions" instance.
 /// </summary>
 public class SearchOptionsFactory : ISearchOptionsFactory
 {
@@ -13,10 +14,16 @@ public class SearchOptionsFactory : ISearchOptionsFactory
     private readonly IMapper<SearchSettingsOptions, SearchOptions> _searchOptionsToAzureOptionsMapper;
 
     /// <summary>
-    /// 
+    /// Snapshot and concrete mapper implementation injected in order to support
+    /// rerieval of the targeted configuration snapshop, and mao to the expected
+    /// "T:Azure.Search.Documents.SearchOptions" instance.
     /// </summary>
-    /// <param name="searchSettingsOptions"></param>
-    /// <param name="searchOptionsToAzureOptionsMapper"></param>
+    /// <param name="searchSettingsOptions">
+    /// The internally configured options.
+    /// </param>
+    /// <param name="searchOptionsToAzureOptionsMapper">
+    /// The search options required to successfully perform an Azure search.
+    /// </param>
     public SearchOptionsFactory(
         IOptionsSnapshot<SearchSettingsOptions> searchSettingsOptions,
         IMapper<SearchSettingsOptions, SearchOptions> searchOptionsToAzureOptionsMapper)
@@ -26,11 +33,18 @@ public class SearchOptionsFactory : ISearchOptionsFactory
     }
 
     /// <summary>
-    /// 
+    /// Rerieves the "T:Dfe.Data.SearchPrototype.Infrastructure.Options.SearchSettingsOptions"
+    /// instance (if configured) and attempts to map to "T:Azure.Search.Documents.SearchOptions" instance.
     /// </summary>
-    /// <param name="targetCollection"></param>
-    /// <returns></returns>
-    /// <exception cref="ArgumentNullException"></exception>
+    /// <param name="targetCollection">
+    /// The key used to target the configuration block describing the collection under scrutiny.
+    /// </param>
+    /// <returns>
+    /// A configured instance of "T:Azure.Search.Documents.SearchOptions".
+    /// </returns>
+    /// <exception cref="ArgumentNullException">
+    /// Exception thrown if no configured target collection key is provided.
+    /// </exception>
     public SearchOptions? GetSearchOptions(string targetCollection)
     {
         if (string.IsNullOrWhiteSpace(targetCollection))
