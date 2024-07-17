88e47c6044d7dae32ef28e517d79ec1d66a9c509
Antony OAKES <Antony.OAKES@education.gov.uk>
2024-07-17 10:40:33 +0100

    moved search compoenent into pageobject model dir

---
+++ Dfe.Data.SearchPrototype/Web/Tests/PageObjectModel/PageComponents/SearchComponent.cs
@@ -0,0 +1,42 @@
+﻿using AngleSharp.Dom;
+using AngleSharp.Html.Dom;
+
+namespace Dfe.Data.SearchPrototype.Web.Tests.PageObjectModel.PageComponents
+{
+    public sealed class SearchComponent : PageComponent
+    {
+        private const string SearchElementTag = "form";
+
+        private SearchComponent(IDocument documentObjectModel)
+            : base(documentObjectModel, SearchElementTag)
+        {
+        }
+
+        public static SearchComponent Create(IDocument documentObjectModel) => new(documentObjectModel);
+
+        public IHtmlInputElement GetTextInputBox(string tagName) =>
+        PageElement == null ?
+                throw new InvalidOperationException(
+                    "Unable to derive the search input box.") :
+                (IHtmlInputElement)PageElement
+                    .GetElementsByTagName(tagName).Single(); // TODO try to get by the name
+
+        public async Task ClickSubmitAsync()
+        {
+            var whatOnEarth = (IHtmlFormElement)PageElement;
+            whatOnEarth.SubmitAsync();
+
+
+            //Task.Run(() => {
+            //    ((IHtmlFormElement)PageElement!).SubmitAsync();
+            //}).Wait();
+        }
+
+        public IHtmlButtonElement GetSubmitButton(string tagName) =>
+        PageElement == null ?
+                throw new InvalidOperationException(
+                    "Unable to derive the search submit button.") :
+                (IHtmlButtonElement)PageElement
+                    .GetElementsByTagName(tagName).Single();
+    }
+}
