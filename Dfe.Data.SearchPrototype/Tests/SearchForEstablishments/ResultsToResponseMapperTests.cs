7b0db21dfdd49d5d8be19414813fe6c2c8f3450a
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-07-17 14:44:37 +0100

    Add test doubles to results to response mapper test

--- Dfe.Data.SearchPrototype/Tests/SearchForEstablishments/ResultsToResponseMapperTests.cs
+++ Dfe.Data.SearchPrototype/Tests/SearchForEstablishments/ResultsToResponseMapperTests.cs
@@ -1,5 +1,6 @@
 ﻿using Dfe.Data.SearchPrototype.Search;
 using Dfe.Data.SearchPrototype.SearchForEstablishments;
+using Dfe.Data.SearchPrototype.Tests.SearchForEstablishments.TestDoubles;
 using DfE.Data.ComponentLibrary.CrossCuttingConcerns.Mapping;
 using FluentAssertions;
 using Xunit;
@@ -12,8 +13,7 @@ public class ResultsToResponseMapperTests
     public void MapFrom_ValidInput_ReturnsCorrectResponse()
     {
         // arrange.
-        EstablishmentResults input = new EstablishmentResults();
-        input.AddEstablishment(new Establishment("urn", "123456"));
+        EstablishmentResults input = EstablishmentResultsTestDouble.Create();
 
         // act.
         IMapper<EstablishmentResults, SearchByKeywordResponse> mapper = new ResultsToResponseMapper();
@@ -21,7 +21,7 @@ public class ResultsToResponseMapperTests
 
         //assert.
         response.Should().NotBeNull();
-        response.EstablishmentResults.Should().HaveCount(1);
+        response.EstablishmentResults.Should().HaveCountGreaterThanOrEqualTo(1);
         response.EstablishmentResults.First().Urn.Should().Be(input.Establishments.First().Urn);
         response.EstablishmentResults.First().Name.Should().Be(input.Establishments.First().Name);
     }
