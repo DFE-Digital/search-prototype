3068473126c285a40bc7d237137e22739831a9dc
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-07-17 14:37:55 +0100

    Add test doubles to searchforestablishments

---
+++ Dfe.Data.SearchPrototype/Tests/SearchForEstablishments/TestDoubles/EstablishmentResultsTestDouble.cs
@@ -0,0 +1,28 @@
+﻿using Dfe.Data.SearchPrototype.Search;
+
+namespace Dfe.Data.SearchPrototype.Tests.SearchForEstablishments.TestDoubles;
+
+public static class EstablishmentResultsTestDouble
+{
+    private static string GetEstablishmentNameFake() =>
+             new Bogus.Faker().Company.CompanyName();
+
+    private static string GetEstablishmentIdentifierFake() =>
+        new Bogus.Faker().Random.Int(100000, 999999).ToString();
+
+    public static EstablishmentResults Create()
+    {
+        var establishmentResults = new EstablishmentResults();
+
+        for (int i = 0; i < new Bogus.Faker().Random.Int(1, 10); i++)
+        {
+            establishmentResults.AddEstablishment(new Establishment(GetEstablishmentIdentifierFake(), GetEstablishmentNameFake()));
+        }
+        return establishmentResults;
+    }
+
+    public static EstablishmentResults CreateWithNoResults()
+    {
+        return new EstablishmentResults();
+    }
+}
