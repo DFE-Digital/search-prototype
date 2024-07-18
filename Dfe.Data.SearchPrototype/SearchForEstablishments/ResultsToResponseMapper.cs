9419c75118860a9c80024687e1e70f810cbd0f16
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-07-18 10:32:27 +0100

    Comment ResultsToResonseMapper

--- Dfe.Data.SearchPrototype/SearchForEstablishments/ResultsToResponseMapper.cs
+++ Dfe.Data.SearchPrototype/SearchForEstablishments/ResultsToResponseMapper.cs
@@ -4,15 +4,22 @@ using DfE.Data.ComponentLibrary.CrossCuttingConcerns.Mapping;
 namespace Dfe.Data.SearchPrototype.SearchForEstablishments;
 
 /// <summary>
-/// 
+/// Facilitates mapping from the received T:Dfe.Data.SearchPrototype.Search.EstablishmentResults
+/// into the required T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordResponse object.
 /// </summary>
 public class ResultsToResponseMapper : IMapper<EstablishmentResults, SearchByKeywordResponse>
 {
     /// <summary>
-    /// 
+    /// The mapping input is the T:Dfe.Data.SearchPrototype.Search.EstablishmentResults which, if successful,
+    /// a new T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordResponse
+    /// instance is created.
     /// </summary>
-    /// <param name="input"></param>
-    /// <returns></returns>
+    /// <param name="input">
+    /// A configured T:Dfe.Data.SearchPrototype.Search.EstablishmentResults instance.
+    /// </param>
+    /// <returns>
+    /// A configured T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordResponse instance.
+    /// </returns>
     public SearchByKeywordResponse MapFrom(EstablishmentResults input)
     {
        SearchByKeywordResponse response = new(input.Establishments);
