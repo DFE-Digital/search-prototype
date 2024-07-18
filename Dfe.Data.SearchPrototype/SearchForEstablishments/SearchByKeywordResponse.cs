bbdcfdd52b451e6aeecd4888ba7db097f255100a
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-07-18 10:47:29 +0100

    Add comments to SearchByKeywordResponse

--- Dfe.Data.SearchPrototype/SearchForEstablishments/SearchByKeywordResponse.cs
+++ Dfe.Data.SearchPrototype/SearchForEstablishments/SearchByKeywordResponse.cs
@@ -3,19 +3,24 @@
 namespace Dfe.Data.SearchPrototype.SearchForEstablishments;
 
 /// <summary>
-/// 
+/// This is the object that carries the reponse (output) back from the
+/// T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordUseCase instance.
+/// The response will encapsulate any search results found along with a status.
 /// </summary>
 public sealed class SearchByKeywordResponse
 {
     /// <summary>
-    /// 
+    /// The readonly collection of T:Dfe.Data.SearchPrototype.Search.Establishment search results.
     /// </summary>
     public IReadOnlyCollection<Establishment> EstablishmentResults { get;}
 
     /// <summary>
-    /// 
+    /// The following argument is passed via the constructor and is not changeable
+    /// once an instance is created, this ensures we preserve immutability.
     /// </summary>
-    /// <param name="establishments"></param>
+    /// <param name="establishments">
+    /// The readonly collection of T:Dfe.Data.SearchPrototype.Search.Establishment search results.
+    /// </param>
     public SearchByKeywordResponse(IReadOnlyCollection<Establishment> establishments)
     {
         EstablishmentResults = establishments;
