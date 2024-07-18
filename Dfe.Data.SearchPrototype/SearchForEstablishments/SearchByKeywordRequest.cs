2c5ce6cf10fa1e4328e2931be5ab56c4a0d0ca09
AsiaWi <123170965+AsiaWi@users.noreply.github.com>
2024-07-18 10:39:51 +0100

    Add comments to SearchByKeword Request

--- Dfe.Data.SearchPrototype/SearchForEstablishments/SearchByKeywordRequest.cs
+++ Dfe.Data.SearchPrototype/SearchForEstablishments/SearchByKeywordRequest.cs
@@ -3,12 +3,14 @@
 namespace Dfe.Data.SearchPrototype.SearchForEstablishments;
 
 /// <summary>
-/// 
+/// This is the object used to make requests (send input) through to the 
+/// T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordUseCase instance.
 /// </summary>
 public sealed class SearchByKeywordRequest
 {
     /// <summary>
-    /// 
+    /// This property exposes the T:Dfe.Data.SearchPrototype.Search.SearchContext object
+    /// which encapsulates the criteria necessary to perform a valid search.
     /// </summary>
     public SearchContext? Context { get; set; }
 }
