ac797110a028c09443fd1d3d7709ee1614ba274e
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-07-22 14:51:14 +0100

    moved logic from view to view model

--- Dfe.Data.SearchPrototype/Web/Models/SearchResultsViewModel.cs
+++ Dfe.Data.SearchPrototype/Web/Models/SearchResultsViewModel.cs
@@ -3,4 +3,8 @@
 public class SearchResultsViewModel
 {
     public List<SearchItemViewModel>? SearchItems { get; set; }
+
+    public bool HasResults => SearchResultsCount >= 1;
+    public bool HasMoreThanOneResult => SearchResultsCount > 1;
+    public int SearchResultsCount => SearchItems == null ? 0 : SearchItems.Count;
  }
