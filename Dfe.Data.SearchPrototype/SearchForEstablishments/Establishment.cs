82839275e78681446bcbddb4c2000d93fcc23c73
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-07-29 09:05:34 +0100

    establishment model mods

--- Dfe.Data.SearchPrototype/SearchForEstablishments/Establishment.cs
+++ Dfe.Data.SearchPrototype/SearchForEstablishments/Establishment.cs
@@ -10,15 +10,16 @@ public class Establishment
     /// <summary>
     /// The read-only URN (unique identifier) of the given establishment.
     /// </summary>
-    public string Urn { get; init; }
+    public string Urn { get; }
     /// <summary>
     /// The read-only name associated with the given establishment.
     /// </summary>
-    public string Name { get; init; }
+    public string Name { get; }
     /// <summary>
     /// The read-only address associated with the given establishment
     /// </summary>
-    public Address Address { get; init; }
+    public Address Address { get; }
+
     /// <summary>
     /// Establishes an immutable establishment instance via the constructor arguments specified.
     /// </summary>
