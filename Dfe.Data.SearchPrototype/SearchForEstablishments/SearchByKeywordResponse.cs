02c6a6b28849f0407a4a1e60f2661a840cf3565b
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-08-29 09:06:50 +0100

    remove warnings

--- Dfe.Data.SearchPrototype/SearchForEstablishments/SearchByKeywordResponse.cs
+++ Dfe.Data.SearchPrototype/SearchForEstablishments/SearchByKeywordResponse.cs
@@ -2,22 +2,64 @@
 
 namespace Dfe.Data.SearchPrototype.SearchForEstablishments;
 
+/// <summary>
+/// The object that encapsulates a single facet result for a facet 
+/// </summary>
 public class FacetResult
 {
-    public int Count { get; set; }
-    public string Value { get; set; }
+    /// <summary>
+    /// The value of the facet result
+    /// </summary>
+    public string Value { get; }
+
+    /// <summary>
+    /// The number of records that belong to this facet value
+    /// </summary>
+    public int? Count { get; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="value">The value of the facet result</param>
+    /// <param name="count">The number of records that belong to this facet value</param>
+    public FacetResult(string value, int? count)
+    {
+        Value = value;
+        Count = count;
+    }
 }
 
+/// <summary>
+/// The object that encapsulates the Faceted results returned by the
+/// T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordUseCase instance
+/// </summary>
 public class EstablishmentFacet
 {
-    public string Name { get; set; } // e.g. ESTABLISHMENTSTATUS
-    public IList<FacetResult> Results { get; } = new List<FacetResult>(); // e.g. "open", count
+    /// <summary>
+    /// The facet (field) name
+    /// </summary>
+    public string Name { get; }
+
+    /// <summary>
+    /// The collection of T:Dfe.Data.SearchPrototype.Search.FacetResult
+    /// </summary>
+    public IList<FacetResult> Results { get; }
+
+    /// <summary>
+    /// Constructor
+    /// </summary>
+    /// <param name="facetName"></param>
+    /// <param name="facetResults"></param>
+    public EstablishmentFacet(string facetName, IList<FacetResult> facetResults)
+    {
+        Name = facetName;
+        Results = facetResults;
+    }
 }
 
 /// <summary>
 /// This is the object that carries the response (output) back from the
 /// T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordUseCase instance.
-/// The response will encapsulate any search results found along with a status.
 /// </summary>
 public sealed class SearchByKeywordResponse
 {
@@ -26,8 +68,15 @@ public sealed class SearchByKeywordResponse
     /// </summary>
     public IReadOnlyCollection<Establishment> EstablishmentResults { get;}
 
-    public IReadOnlyCollection<EstablishmentFacet> EstablishmentFacetResults { get; }
+    /// <summary>
+    /// The readonly collection of T:Dfe.Data.SearchPrototype.Search.EstablishmentFacet returned by the Establishment search
+    /// </summary>
+    public IReadOnlyCollection<EstablishmentFacet>? EstablishmentFacetResults { get; }
 
+    /// <summary>
+    /// The return status of the call to the
+    /// T:Dfe.Data.SearchPrototype.SearchForEstablishments.SearchByKeywordUseCase instance
+    /// </summary>
     public SearchResponseStatus Status { get; set; }
 
     /// <summary>
@@ -45,8 +94,12 @@ public sealed class SearchByKeywordResponse
     /// <param name="establishments">
     /// The readonly collection of T:Dfe.Data.SearchPrototype.Search.Establishment search results.
     /// </param>
-    public SearchByKeywordResponse(IReadOnlyCollection<Establishment> establishments)
+    /// <param name="facetResults">
+    /// The readonly collection of T:Dfe.Data.SearchPrototype.Search.EstablishmentFacet
+    /// </param>
+    public SearchByKeywordResponse(IReadOnlyCollection<Establishment> establishments, IReadOnlyCollection<EstablishmentFacet>? facetResults = null)
     {
         EstablishmentResults = establishments;
+        EstablishmentFacetResults = facetResults;
     }
 }
