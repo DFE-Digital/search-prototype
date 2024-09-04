d50d8a74d83b4d8736e5f3d1a92e74c1432ce2b3
Catherine Lawlor <55924986+CathLass@users.noreply.github.com>
2024-09-04 16:57:15 +0100

    Cl/service registration methods (#43)
    * add extension methods to register dependencies

---
+++ Dfe.Data.SearchPrototype/SearchForEstablishments/DependencyRegistration.cs
@@ -0,0 +1,20 @@
+﻿using Dfe.Data.SearchPrototype.Common.CleanArchitecture.Application.UseCase;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Dfe.Data.SearchPrototype.SearchForEstablishments;
+
+/// <summary>
+/// Extension method which provides all the pre-registrations required to
+/// use the SearchForEstablishments services
+/// </summary>
+public static class DependencyRegistration
+{
+    /// <summary>
+    /// Register all the necessary SearchForEstablishments services
+    /// </summary>
+    /// <param name="services"></param>
+    public static void AddSearchForEstablishmentServices(this IServiceCollection services)
+    {
+        services.AddScoped<IUseCase<SearchByKeywordRequest, SearchByKeywordResponse>, SearchByKeywordUseCase>();
+    }
+}
