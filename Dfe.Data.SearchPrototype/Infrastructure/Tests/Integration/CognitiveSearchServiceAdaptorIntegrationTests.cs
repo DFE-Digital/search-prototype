3d36c1a91171d5b6c0eb2d9ef88439bc60575d30
CLAWLOR <catherine.lawlor@education.gov.uk>
2024-09-26 11:40:51 +0100

    move configuration and setup into classes for use in ClassFixture

--- Dfe.Data.SearchPrototype/Infrastructure/Tests/Integration/CognitiveSearchServiceAdaptorIntegrationTests.cs
+++ Dfe.Data.SearchPrototype/Infrastructure/Tests/Integration/CognitiveSearchServiceAdaptorIntegrationTests.cs
@@ -1,25 +1,24 @@
 ﻿using Azure;
 using Azure.Search.Documents.Models;
-using Dfe.Data.Common.Infrastructure.CognitiveSearch;
 using Dfe.Data.Common.Infrastructure.CognitiveSearch.Filtering;
 using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword;
 using Dfe.Data.SearchPrototype.Common.Mappers;
 using Dfe.Data.SearchPrototype.Infrastructure.Options;
+using Dfe.Data.SearchPrototype.Infrastructure.Tests.Integration.TestHarness;
 using Dfe.Data.SearchPrototype.Infrastructure.Tests.TestDoubles;
 using Dfe.Data.SearchPrototype.Infrastructure.Tests.TestDoubles.Shared;
 using Dfe.Data.SearchPrototype.SearchForEstablishments.Models;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.DependencyInjection.Extensions;
-using Moq;
-using System.Runtime.CompilerServices;
 using Xunit;
 
 namespace Dfe.Data.SearchPrototype.Infrastructure.Tests.Integration;
 
-public class CognitiveSearchServiceAdaptorIntegrationTests
+public class CognitiveSearchServiceAdaptorIntegrationTests :IClassFixture<ConfigBuilder>, IClassFixture<MyServiceProvider>
 {
     private AzureSearchOptions _options = AzureSearchOptionsTestDouble.Stub();
+    private IConfiguration _configuration;
+    private IServiceProvider _serviceProvider;
 
     private static Dictionary<string, string> InMemoryConfig => new Dictionary<string, string> {
             //{"FilterKeyToFilterExpressionMapOptions:FilterChainingLogicalOperator", "AndLogicalOperator"},
@@ -27,59 +26,27 @@ public class CognitiveSearchServiceAdaptorIntegrationTests
             //{"FilterKeyToFilterExpressionMapOptions:SearchFilterToExpressionMap:ESTABLISHMENTSTATUSNAME", "SearchInFilterExpression" }
         };
 
+    public CognitiveSearchServiceAdaptorIntegrationTests(ConfigBuilder configBuilder, MyServiceProvider serviceProvider)
+    {
+        _configuration = configBuilder.SetupConfiguration(InMemoryConfig);
+        _serviceProvider = serviceProvider.SetUpServiceProvider(_configuration);
+    }
+
     [Fact]
     public async Task  CompositionRoot_DoesEverythingItNeeds()
     {
-        // configuration
-        IConfiguration config = SetupConfiguration(InMemoryConfig);
-
-        // services
-        var serviceProvider = SetUpServiceProvider(config);
-
         // Create system under test
         var adapter = new CognitiveSearchServiceAdapter<DataTransferObjects.Establishment>(
-            serviceProvider.GetRequiredService<ISearchByKeywordService>(),
+            _serviceProvider.GetRequiredService<ISearchByKeywordService>(),
             IOptionsTestDouble.IOptionsMockFor(_options),
-            serviceProvider.GetRequiredService<IMapper<Pageable<SearchResult<DataTransferObjects.Establishment>>, EstablishmentResults>>(),
-            serviceProvider.GetRequiredService<IMapper<Dictionary<string, IList<Azure.Search.Documents.Models.FacetResult>>, EstablishmentFacets>>(),
-            serviceProvider.GetRequiredService<ISearchFilterExpressionsBuilder>()
+            _serviceProvider.GetRequiredService<IMapper<Pageable<SearchResult<DataTransferObjects.Establishment>>, EstablishmentResults>>(),
+            _serviceProvider.GetRequiredService<IMapper<Dictionary<string, IList<Azure.Search.Documents.Models.FacetResult>>, EstablishmentFacets>>(),
+            _serviceProvider.GetRequiredService<ISearchFilterExpressionsBuilder>()
             );
 
         var searchServiceAdapterRequest = SearchServiceAdapterRequestTestDouble.Create();
 
         // act
         var response = await adapter.SearchAsync(searchServiceAdapterRequest);
-    }
-
-    private IServiceProvider SetUpServiceProvider(IConfiguration config)
-    {
-        IServiceCollection services = new ServiceCollection();
-        services.AddSingleton<IConfiguration>(config);
-        services.AddCognitiveSearchAdaptorServices(config);
-        //services.AddDefaultSearchFilterServices(config);
-
-        // replace Common.Infrastructure services with mocks
-        services.RemoveAll<ISearchByKeywordService>();
-        var mockSearchService = new SearchServiceMockBuilder()
-            .WithSearchKeywordAndCollection("searchKeyword","establishments")
-            .WithSearchResults(new SearchResultFakeBuilder()
-                .WithSearchResults()
-                .Create())
-            .Create();
-        services.AddScoped<ISearchByKeywordService>(provider => mockSearchService);
-
-        services.RemoveAll<ISearchFilterExpressionsBuilder>();
-        var mockFilterExpressionBuilder = new FilterExpressionBuilderTestDouble().Create();
-        services.AddScoped<ISearchFilterExpressionsBuilder>(provider => mockFilterExpressionBuilder);
-
-        return services.BuildServiceProvider();
-    }
-
-    private IConfiguration SetupConfiguration(Dictionary<string, string> options)
-    {
-        var configBuilder = new ConfigurationBuilder();
-
-        configBuilder.AddInMemoryCollection(options);
-        return configBuilder.Build();
     }
 }
---
+++ Dfe.Data.SearchPrototype/Infrastructure/Tests/Integration/TestHarness/MyServiceProvider.cs
@@ -0,0 +1,35 @@
+﻿using Dfe.Data.Common.Infrastructure.CognitiveSearch.Filtering;
+using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword;
+using Dfe.Data.SearchPrototype.Infrastructure.Tests.TestDoubles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Dfe.Data.SearchPrototype.Infrastructure.Tests.Integration.TestHarness;
+
+public class MyServiceProvider
+{
+    public IServiceProvider SetUpServiceProvider(IConfiguration config)
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddSingleton<IConfiguration>(config);
+        services.AddCognitiveSearchAdaptorServices(config);
+        //services.AddDefaultSearchFilterServices(config);
+
+        // replace Common.Infrastructure services with mocks
+        services.RemoveAll<ISearchByKeywordService>();
+        var mockSearchService = new SearchServiceMockBuilder()
+            .WithSearchKeywordAndCollection("searchKeyword", "establishments")
+            .WithSearchResults(new SearchResultFakeBuilder()
+                .WithSearchResults()
+                .Create())
+            .Create();
+        services.AddScoped<ISearchByKeywordService>(provider => mockSearchService);
+
+        services.RemoveAll<ISearchFilterExpressionsBuilder>();
+        var mockFilterExpressionBuilder = new FilterExpressionBuilderTestDouble().Create();
+        services.AddScoped<ISearchFilterExpressionsBuilder>(provider => mockFilterExpressionBuilder);
+
+        return services.BuildServiceProvider();
+    }
+}
