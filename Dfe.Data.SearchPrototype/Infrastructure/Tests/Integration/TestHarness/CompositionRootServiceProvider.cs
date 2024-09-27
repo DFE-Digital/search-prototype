e2804b3c0896dd7187da37ae60d6b0bd78fa76c8
Catherine Lawlor <55924986+CathLass@users.noreply.github.com>
2024-09-27 13:10:08 +0100

    Cl/infrastructure composition root tests (#51)
    * Test CompositionRoot for infrastucture stuff - mocking Common.Infrastucture services
    
    * move configuration and setup into classes for use in ClassFixture
    
    * test everything in the composition root
    
    * rename MyServiceProvider :)

---
+++ Dfe.Data.SearchPrototype/Infrastructure/Tests/Integration/TestHarness/CompositionRootServiceProvider.cs
@@ -0,0 +1,35 @@
+﻿using Dfe.Data.Common.Infrastructure.CognitiveSearch.Filtering;
+using Dfe.Data.Common.Infrastructure.CognitiveSearch.SearchByKeyword;
+using Dfe.Data.SearchPrototype.Infrastructure.Tests.TestDoubles;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+
+namespace Dfe.Data.SearchPrototype.Infrastructure.Tests.Integration.TestHarness;
+
+public class CompositionRootServiceProvider
+{
+    public IServiceProvider SetUpServiceProvider(IConfiguration config)
+    {
+        IServiceCollection services = new ServiceCollection();
+        services.AddSingleton<IConfiguration>(config);
+
+        // this is the extension method to add all the dependencies
+        services.AddCognitiveSearchAdaptorServices(config);
+
+        // Replace Common.Infrastructure services with mocks
+        services.RemoveAll<ISearchByKeywordService>();
+        var mockSearchService = new SearchServiceMockBuilder()
+            .WithSearchKeywordAndCollection("searchKeyword", "establishments")
+            .WithSearchResults(new SearchResultFakeBuilder()
+                .WithSearchResults()
+                .Create())
+            .Create();
+        services.AddScoped<ISearchByKeywordService>(provider => mockSearchService);
+        services.RemoveAll<ISearchFilterExpressionsBuilder>();
+        var mockFilterExpressionBuilder = new FilterExpressionBuilderTestDouble().Create();
+        services.AddScoped<ISearchFilterExpressionsBuilder>(provider => mockFilterExpressionBuilder);
+
+        return services.BuildServiceProvider();
+    }
+}
